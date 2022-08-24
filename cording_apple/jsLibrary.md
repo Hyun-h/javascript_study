62c4521252b7201d8efa7216e858e1502c9e03ab
홍현아 <hyeonahong@gmail.com>
2022-08-24 09:39:13 +0900

    상품 상세페이지 분리

---
+++ cording_apple/jsLibrary.md
@@ -0,0 +1,7 @@
+자바스크립트 라이브러리
+
+1. Swiper : 캐러셀 라이브러리
+2. Chart.js : 차트 라이브러리
+3. Animation On Scroll : 스크롤 애니메이션 라이브러리
+4. EmailJS : 자바스크립트로 이메일 전송하는 라이브러리
+5. Fullpage.js : 풀페이지 관련 라이브러리
--- cording_apple/기초강의/index.js
+++ cording_apple/기초강의/index.js
@@ -167,26 +167,3 @@ terms.addEventListener('scroll', () => {
         alertOnce = true;
     }
 });
-
-//tab menu
-const tabButtons = document.querySelectorAll('.tab-button');
-const tabContents = document.querySelectorAll('.tab-content');
-
-//이벤트 내부에서 반복되는 forEach 분리해서 함수로 만들기
-function isTabActive(selectors, className) {
-    selectors.forEach((item) => {
-        item.classList.remove(className);
-    });
-}
-
-//tab nodeList 반복문
-tabButtons.forEach((button, index) => {
-    //nodeList 배열의 요소가 클릭되는 이벤트
-    button.addEventListener('click', () => {
-        isTabActive(tabButtons, 'orange');
-        button.classList.add('orange');
-
-        isTabActive(tabContents, 'show');
-        tabContents[index].classList.add('show');
-    });
-});
