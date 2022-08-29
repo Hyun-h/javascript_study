cc583b1accab7aec79aacc2b33de6c3842ce4e7b
홍현아 <hyeonahong@gmail.com>
2022-08-29 17:56:49 +0900

    localStorage 이론, TODO 등록

--- cording_apple/기초강의/detail.js
+++ cording_apple/기초강의/detail.js
@@ -170,3 +170,49 @@ tabButtons.forEach((button, index) => {
     tabContents[index].classList.add("show");
   });
 });
+
+//localStorage
+//등록
+localStorage.setItem("이름", "홍길동");
+//출력
+localStorage.getItem("이름");
+console.log(localStorage.getItem("이름"));
+//삭제
+localStorage.removeItem("이름");
+
+/*
+localStorage에 array, object 저장하기.
+array나 object를 그냥 위에처럼 저장하면 문자열로 강제로 변환이 되면서 깨져서 저장이 됨.
+
+1. 변수에 원하는 형태를 담기
+2. array/object -> JSON으로 바꾸기
+3. localStorage에 등록하기
+
+만약 sessionStorage를 쓰고 싶으면 local -> session으로 변경
+*/
+const localArr = [1, 2, 3];
+const NewLocalArr = JSON.stringify(localArr);
+
+localStorage.setItem("num", NewLocalArr);
+
+//꺼낼 때도 그대로 꺼내면 문자열로 꺼내지게 되어서 JSON 변환 필요.
+const 꺼낸거 = localStorage.getItem("num");
+const 꺼낸거배열 = JSON.parse(꺼낸거);
+console.log(꺼낸거배열[1]);
+
+/*
+localStorage 데이터 수정
+
+1. 자료꺼냄
+2. 꺼낸 거 수정하기
+3. 다시 넣음
+
+TODO: 구매버튼 누르면 누른 상품명 localStorage에 넣기
+key: cart, Value: ["product1", "product2"]
+팁1: 내 형제요소(sibling) 찾는 법 알아내기
+팁2: localStorage가 비어있을 때 / 뭐가 있을 때 저장방식이 다름.
+    구매버튼을 누를 때 cart 항목이 없으면 array 추가해야 함.
+    구매버튼을 누를 때 cart 항목이 없으면 array 수정해야 함.
+
+TODO: cart.html 방문 시 localStorage에 저장된 상품명들 다 보여주기
+*/
