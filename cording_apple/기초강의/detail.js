209413b4833cf3369219c7fb3e0d69e0e8ffa63b
홍현아 <hyeonahong@gmail.com>
2022-08-29 17:25:12 +0900

    detail.js 컨텐츠 순서대로 파일 위치 변경, 상품 목록에 버튼 추가

--- cording_apple/기초강의/detail.js
+++ cording_apple/기초강의/detail.js
@@ -1,35 +1,3 @@
-//tab menu
-const tabButtons = document.querySelectorAll(".tab-button");
-const tabContents = document.querySelectorAll(".tab-content");
-
-//이벤트 내부에서 반복되는 forEach 분리해서 함수로 만들기
-function isTabActive(selectors, className) {
-  selectors.forEach((item) => {
-    item.classList.remove(className);
-  });
-}
-
-//tab nodeList 반복문
-tabButtons.forEach((button, index) => {
-  //nodeList 배열의 요소가 클릭되는 이벤트
-  button.addEventListener("click", () => {
-    isTabActive(tabButtons, "orange");
-    button.classList.add("orange");
-
-    isTabActive(tabContents, "show");
-    tabContents[index].classList.add("show");
-  });
-});
-
-//object에서 자료 뽑아서 변경
-const car = {
-  name: "소나타",
-  price: [50000, 20000, 40000],
-};
-
-document.getElementById("product-name").innerText = car.name;
-document.getElementById("product-price").innerText = car.price[0];
-
 /*
 TODO:
 더보기를 눌러서 목록이 불러와졌을 때도 정렬 기능 3가지가 작동하게 하기
@@ -96,6 +64,7 @@ function setCard(product) {
       <img src="https://via.placeholder.com/600" class="w-100">
       <h5>${product.title}</h5>
       <p>가격 : ${product.price}</p>
+      <button class="btn btn-dark buy">구매</button>
     </div>
     `;
 
@@ -168,4 +137,36 @@ productSelect.addEventListener("change", (e) => {
       optionSelect.innerHTML += `<option>${pantsSize}</option>`;
     });
   }
+});
+
+//object에서 자료 뽑아서 변경
+const car = {
+  name: "소나타",
+  price: [50000, 20000, 40000],
+};
+
+document.getElementById("product-name").innerText = car.name;
+document.getElementById("product-price").innerText = car.price[0];
+
+//tab menu
+const tabButtons = document.querySelectorAll(".tab-button");
+const tabContents = document.querySelectorAll(".tab-content");
+
+//이벤트 내부에서 반복되는 forEach 분리해서 함수로 만들기
+function isTabActive(selectors, className) {
+  selectors.forEach((item) => {
+    item.classList.remove(className);
+  });
+}
+
+//tab nodeList 반복문
+tabButtons.forEach((button, index) => {
+  //nodeList 배열의 요소가 클릭되는 이벤트
+  button.addEventListener("click", () => {
+    isTabActive(tabButtons, "orange");
+    button.classList.add("orange");
+
+    isTabActive(tabContents, "show");
+    tabContents[index].classList.add("show");
+  });
 });
