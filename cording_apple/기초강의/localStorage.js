d2f609b18a458ac16058270613639785105f3a4b
홍현아 <hyeonahong@gmail.com>
2022-09-05 14:29:19 +0900

    중복된 상품이 있을 경우 나열하는 게 아닌 몇 개인지 구현

--- cording_apple/기초강의/localStorage.js
+++ cording_apple/기초강의/localStorage.js
@@ -1,66 +1,47 @@
-/*
-localStorage에 array, object 저장하기.
-array나 object를 그냥 위에처럼 저장하면 문자열로 강제로 변환이 되면서 깨져서 저장이 됨.
-
-1. 변수에 원하는 형태를 담기
-2. array/object -> JSON으로 바꾸기
-3. localStorage에 등록하기
-
-만약 sessionStorage를 쓰고 싶으면 local -> session으로 변경
-*/
-const localArr = [1, 2, 3];
-const NewLocalArr = JSON.stringify(localArr);
-
-localStorage.setItem("num", NewLocalArr);
-
-//꺼낼 때도 그대로 꺼내면 문자열로 꺼내지게 되어서 JSON 변환 필요.
-const 꺼낸거 = localStorage.getItem("num");
-const 꺼낸거배열 = JSON.parse(꺼낸거);
-console.log(꺼낸거배열[1]);
-
-/*
-localStorage 데이터 수정
-
-1. 자료꺼냄
-2. 꺼낸 거 수정하기
-3. 다시 넣음
-*/
-
-/*
-로컬스토리지는 저장이 되지만, 배열은 로딩이 될 때마다 휘발이 된다.
-로컬스토리지와 배열의 개념적 기계가 충돌하는 정신 모델을 만들어서 구조 자체가 잘못 구현됨.
-
-TODO: 중복된 상품이 있을 경우 나열하는 게 아닌 몇 개인지 구현
-*/
-
 const CART_KEY = "cart";
-const savedCartItem = localStorage.getItem(CART_KEY);
 
-let cartItems = [];
+function saveCartItem(newCartItemObj) {
+  //저장된 로컬스토리지 값 제이슨으로 불러오기
+  const savedCartItem = localStorage.getItem(CART_KEY);
+  let cartItems = [];
 
-function saveCartItem() {
-  //key: cart기 있느냐 없느냐에 따라 수정 / 새로 저장
   if (savedCartItem !== null) {
     const parsedCartItems = JSON.parse(savedCartItem);
-    const changeCartItems = [...parsedCartItems, ...cartItems];
-    localStorage.setItem(CART_KEY, JSON.stringify(changeCartItems));
-  } else {
+    //some()으로 원하는 값이 존재하는지 참, 거짓으로 판별
+    const isParsedItem = parsedCartItems.some((item) => {
+      return item.title === newCartItemObj.title;
+    });
+    //로컬스토리지의 값과 비교했을 때 중복이면 count만 수정
+    if (isParsedItem) {
+      cartItems = parsedCartItems.map((item) => {
+        if (item.title === newCartItemObj.title) {
+          return { ...item, count: item.count + 1 };
+        }
+        return item;
+      });
+    } else {
+      cartItems = [...parsedCartItems, newCartItemObj];
+    }
     localStorage.setItem(CART_KEY, JSON.stringify(cartItems));
+  } else {
+    localStorage.setItem(CART_KEY, JSON.stringify([newCartItemObj]));
   }
 
+  //오브젝트 초기화
+  newCartItemObj = {};
   displayNotification();
 }
 
-//구매버튼 누르면 누른 상품 cartItem에 넣고 saveCartItem 실행
+//구매버튼 누르면 누른 상품이 오브젝트로 생성되고 파라미터로 saveCartItem에 전달
 function handleCartList(e) {
   const productId = e.target.dataset.id;
-  if (productId != undefined) {
+  if (productId !== undefined) {
     const newCartItemObj = {
       title: productData[productId].title,
       price: productData[productId].price,
+      count: 1,
     };
-    cartItems.push(newCartItemObj);
-    saveCartItem();
+    saveCartItem(newCartItemObj);
   }
 }
 
@@ -71,9 +52,11 @@ function displayNotification() {
 }
 
 //localStorage에 cart값이 담긴 게 확인되면 UI 띄우기
-if (savedCartItem !== null) {
+//페이지가 로드되었을 때 로컬스토리지 값
+const initCartItems = localStorage.getItem(CART_KEY);
+if (initCartItems !== null) {
   displayNotification();
-  console.log(JSON.parse(savedCartItem));
+  console.log("initCartItems", JSON.parse(initCartItems));
 }
 
 document.querySelector(".row").addEventListener("click", handleCartList);
