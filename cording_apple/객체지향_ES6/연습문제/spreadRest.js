49b1351c617fdc9360ce99e651fb058a9b690039
홍현아 <hyeonahong@gmail.com>
2022-09-13 11:03:16 +0900

    spread, rest 문제풀이. reduce 활용한 요소의 중복 횟수 구하기

--- cording_apple/객체지향_ES6/연습문제/spreadRest.js
+++ cording_apple/객체지향_ES6/연습문제/spreadRest.js
@@ -8,7 +8,7 @@ console.log(c); //[김, 밥, 1, 2, 3]
 var a = [1, 2, 3];
 var b = ["you", "are"];
 var c = function (a, b) {
-  console.log([[...a], ...[...b]][1]);
+  console.log([[...a], ...[...b]][1]); //you
 };
 c(a, b);
 
@@ -17,17 +17,17 @@ function 함수(a = 5, b = a * 2) {
   console.log(a + b);
   return 10;
 }
-함수(3);
+함수(3); //9
 
 //default 파라미터 문제 2 : 코드의 출력 결과는?
 function 함수(a = 5, b = a * 2) {
   console.log(a + b);
 }
-함수(undefined, undefined);
+함수(undefined, undefined); //undefined
 
 //array 만드는 함수 제작
-function 어레이() {
-  //(여기 어떤코드가 들어가면 될까요?)
+function 어레이(...rest) {
+  return rest;
 }
 
 var newArray = 어레이(1, 2, 3, 4, 5);
@@ -35,10 +35,13 @@ console.log(newArray); //출력예제 : [1,2,3,4,5]
 
 //Math.max를 이용하여 아래 array의 최댓값을 구하는 함수 만들기. 단 괄호 안에 하나하나 넣으면 안됨.
 var numbers = [2, 3, 4, 5, 6, 1, 3, 2, 5, 5, 4, 6, 7];
+const maxNumber = Math.max(...numbers);
+console.log(maxNumber);
 
 //sort()를 활용하여 글자를 알파벳순으로 정렬해주는 함수 만들기
-function 정렬() {
-  //(여기 어떤 코드가 들어가야할까요?)
+function 정렬(text) {
+  const sortText = [...text].sort();
+  console.log(...sortText);
 }
 
 정렬("bear");
@@ -55,3 +58,15 @@ function 정렬() {
 
 글자세기라는 함수를 어떻게 만들면 될까요? 
 */
+function 글자세기(text) {
+  const textArr = [...text].sort();
+
+  const result = textArr.reduce((acc, curr) => {
+    console.log("acc", acc, "curr", acc[curr]);
+    acc[curr] = (acc[curr] || 0) + 1;
+    return acc;
+  }, {});
+  console.log("result", result);
+}
+
+글자세기("aacbbb");
