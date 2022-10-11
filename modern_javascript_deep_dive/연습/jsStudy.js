b19d637137fc31b8c3c91b81bb382ce9a57f1124
홍현아 <hyeonahong@gmail.com>
2022-10-11 16:07:40 +0900

    디스트럭처링 할당 활용

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -1,33 +1,40 @@
-const response = [
-  {
-    country: "KR",
-    province: "all",
-    city: "all",
-    shopList: ["shop1", "shop2", "shop3"],
-  },
-  {
-    country: "CN",
-    province: "all",
-    city: "all",
-    shopList: ["shop4", "shop5", "shop6"],
-  },
-  {
-    country: "US",
-    province: "all",
-    city: "all",
-    shopList: ["shop7", "shop8", "shop9"],
-  },
-];
+// const response = [
+//   {
+//     country: "KR",
+//     province: "all",
+//     city: "all",
+//     shopList: ["shop1", "shop2", "shop3"],
+//   },
+//   {
+//     country: "CN",
+//     province: "all",
+//     city: "all",
+//     shopList: ["shop4", "shop5", "shop6"],
+//   },
+//   {
+//     country: "US",
+//     province: "all",
+//     city: "all",
+//     shopList: ["shop7", "shop8", "shop9"],
+//   },
+// ];
 
-const list = response.flatMap((item) =>
-  item.shopList.map((shop) => {
-    return {
-      city: item.city,
-      country: item.country,
-      province: item.province,
-      shop,
-    };
-  })
-);
+// const list = response.flatMap((item) =>
+//   item.shopList.map((shop) => {
+//     return {
+//       city: item.city,
+//       country: item.country,
+//       province: item.province,
+//       shop,
+//     };
+//   })
+// );
 
-console.log(list);
+// console.log(list);
+
+const el = document.getElementById("result");
+const { className, clientHeight, scrollTop } = el;
+console.log(el);
+console.log(className);
+console.log(clientHeight);
+console.log(scrollTop);
