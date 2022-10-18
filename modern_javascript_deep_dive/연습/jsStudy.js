70c44979c8ca28e54a87087f88fb2969fbbffe5b
홍현아 <hyeonahong@gmail.com>
2022-10-18 14:05:31 +0900

    불리언, undefined, null, 심벌 타입 정리

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -1,18 +1,8 @@
-var binary = 0b1000001; //2진수
-var octal = 0o101; //8진수
-var hex = 0x41; //16진수
+var key = Symbol("key");
+console.log(key);
+console.log(typeof key);
 
-// 표기법만 다르고 모두 같은 값이다.
-console.log(binary);
-console.log(octal);
-console.log(hex);
-console.log(binary === octal);
-console.log(binary === hex);
+var obj = {};
 
-console.log(1 === 1.0);
-console.log(4 / 2);
-console.log(3 / 2);
-
-console.log(10 / 0);
-console.log(10 / -0);
-console.log(1 * "String");
+obj[key] = "value";
+console.log(obj[key]);
