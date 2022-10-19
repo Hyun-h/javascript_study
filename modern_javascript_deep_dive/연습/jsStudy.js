0cd79bf22a087dd5a77e3b9f223b9d5dcacdc59b
홍현아 <hyeonahong@gmail.com>
2022-10-19 12:32:03 +0900

    7.2 할당 연산자 정리

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -1,39 +1,24 @@
-// var x = 1;
+var x;
 
-// x++;
-// console.log(x);
-
-// x--;
-// console.log(x);
-
-// var x = 5,
-//   result;
-
-// result = x++;
-// console.log(result, x);
-
-// result = ++x;
-// console.log(result, x);
-
-// result = x--;
-// console.log(result, x);
-
-// result = --x;
-// console.log(result, x);
-
-var x = "1";
-
-console.log(+x);
+x = 10;
 console.log(x);
 
-x = true;
-console.log(+x);
+x += 5;
 console.log(x);
 
-x = false;
-console.log(+x);
+x -= 5;
 console.log(x);
 
-x = "Hello";
-console.log(+x);
+x *= 5;
 console.log(x);
+
+x /= 5;
+console.log(x);
+
+x %= 5;
+console.log(x);
+
+var str = "My name is ";
+
+str += "Lee";
+console.log(str);
