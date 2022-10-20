e055281ab68238b927448a037efe82aaf499743c
홍현아 <hyeonahong@gmail.com>
2022-10-20 11:41:36 +0900

    8.2 조건문 정리

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -1,24 +1,34 @@
-var x;
+var year = 2000;
+var month = 2;
+var days = 0;
 
-x = 10;
-console.log(x);
+switch (month) {
+  case 1:
+  case 3:
+  case 5:
+  case 7:
+  case 8:
+  case 10:
+  case 12:
+    days = 31;
+    break;
 
-x += 5;
-console.log(x);
+  case 4:
+  case 6:
+  case 9:
+  case 11:
+    days = 30;
+    break;
 
-x -= 5;
-console.log(x);
+  case 2:
+    // 윤년 계산 알고리즘
+    // 1. 연도가 4로 나누어 떨어지는 해(2000, 2004, 2008, 2012, 2016, 2020...)는 윤년이다.
+    // 2. 연도가 4로 나누어 떨어지더라도 연도가 100으로 나누어 떨어지는 해(2000, 2100, 2200)는 평년이다.
+    // 3. 연도가 400으로 나누어 떨어지는 해(2000, 2400, 2800...)는 윤년이다.
+    days = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ? 29 : 28;
+    break;
+  default:
+    console.log("Invalid month");
+}
 
-x *= 5;
-console.log(x);
-
-x /= 5;
-console.log(x);
-
-x %= 5;
-console.log(x);
-
-var str = "My name is ";
-
-str += "Lee";
-console.log(str);
+console.log(days);
