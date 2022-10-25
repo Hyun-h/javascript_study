3a9245c61fda3a421d6c2ad7498be0c39e06df12
홍현아 <hyeonahong@gmail.com>
2022-10-25 15:22:17 +0900

    11.1 원시 값 정리

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -1,49 +1,10 @@
-// var x = 1,
-//   y = 2;
+var score = 80;
+var copy = score;
 
-// var obj = {
-//   x: x,
-//   y: y,
-// };
+console.log(score, copy);
+console.log(score === copy);
 
-// console.log(obj);
+score = 100;
 
-// var prefix = "prop";
-// var i = 0;
-
-// var obj = {};
-
-// obj[prefix + "-" + ++i] = i;
-// obj[prefix + "-" + ++i] = i;
-// obj[prefix + "-" + ++i] = i;
-
-// console.log(obj);
-
-// const prefix = "prop";
-// let i = 0;
-
-// const obj = {
-//   [`${prefix}-${++i}`]: i,
-//   [`${prefix}-${++i}`]: i,
-//   [`${prefix}-${++i}`]: i,
-// };
-
-// console.log(obj);
-
-// var obj = {
-//   name: "Lee",
-//   sayHi: function () {
-//     console.log("Hi! " + this.name);
-//   },
-// };
-
-// obj.sayHi();
-
-const obj = {
-  name: "Lee",
-  sayHi() {
-    console.log(`Hi! ${this.name}`);
-  },
-};
-
-obj.sayHi();
+console.log(score, copy);
+console.log(score === copy);
