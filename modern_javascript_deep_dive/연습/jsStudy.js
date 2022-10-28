8f43120c1b6184395fd7048a6b373f56069621f8
홍현아 <hyeonahong@gmail.com>
2022-10-28 16:52:13 +0900

    12.5 함수 호출 정리

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -1,39 +1,41 @@
-// (function () {
-//   console.log("bar");
-// })();
-
-// var add = function test(x, y) {
-//   return x + y;
-// };
-
-// console.log(add(2, 5)); // 7
-
-// console.dir(add);
-// console.dir(sub);
-
-// console.log(add(2, 5));
-// console.log(sub(2, 5));
-
 // function add(x, y) {
+//   console.log(arguments);
+
 //   return x + y;
 // }
 
-// var sub = function (x, y) {
-//   return x - y;
-// };
+// console.log(add(2, 5, 10)); // 7
 
-// var add1 = (function () {
-//   var a = 10;
-//   return function (x, y) {
-//     return x + y + a;
-//   };
-// })();
+// function add(x, y) {
+//   if (typeof x !== "number" || typeof y !== "number") {
+//     // 매개변수를 통해 전달된 인수의 타입이 부적절한 경우 에러를 발생시킨다.
+//     throw new TypeError("인수는 모두 숫자 값이어야 합니다.");
+//   }
 
-// console.log(add1(1, 2)); // 13
+//   return x + y;
+// }
 
-// var add2 = (function () {
-//   var a = 10;
-//   return new Function("x", "y", "return x + y + a;");
-// })();
+// console.log(add(2));
+// console.log(add("a", "b"));
 
-// console.log(add2(1, 2)); // ReferenceError: a is not defined
+// function add(a, b, c) {
+//   a = a || 0;
+//   b = b || 0;
+//   c = c || 0;
+
+//
+// }
+
+// console.log(add(1, 2, 3));
+// console.log(add(1, 2));
+// console.log(add(1));
+// console.log(add());
+
+function add(a = 0, b = 0, c = 0) {
+  return a + b + c;
+}
+
+console.log(add(1, 2, 3));
+console.log(add(1, 2));
+console.log(add(1));
+console.log(add());
