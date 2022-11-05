181312f7e269863a02a7a24f68f4379e5aac04bf
Hyun-h <hyeonahong@gmail.com>
2022-11-05 17:06:26 +0900

    27.9.2 Array.prototype.forEach 정리

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -1,82 +1,68 @@
-// var x = "global";
+function solution(string) {
+  // 검사할 기준을 데이터로 만든다
+  const bracketsData = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
 
-// function foo() {
-//   var x = "local";
-//   console.log(x);
-// }
+  const openBracket = Object.values(bracketsData);
 
-// foo();
+  // 체크하고 넣을 빈 배열
+  const checkedArr = [];
 
-// console.log(x);
+  // 매개변수로 들어온 문자열을 배열로 만든다
+  const stringArr = [...string];
 
-// var x = "global x";
-// var y = "global y";
+  stringArr.forEach((item) => {
+    // 매개변수로 들어온 배열의 요소(stringArr의 요소)가 데이터의 값(openBracket)과 같은지 확인하고 같으면 checkedArr에 넣는다. (추가)
+    if (openBracket.includes(item)) {
+      checkedArr.push(item);
+    } else {
+      // checkedArr의 요소와 data의 값이 일치하면 checkedArr에서 일치하는 요소를 지운다. (추가된 요소 삭제)
+      if (checkedArr.includes(bracketsData[item])) {
+        checkedArr.pop(bracketsData[item]);
+      } else {
+        // 모든 조건문을 돌고 나서 조건에 맞지 않는 요소는 집어넣기
+        checkedArr.push(item);
+      }
+    }
+  });
 
-// function outer() {
-//   var z = "outer's local z";
-
-//   console.log(x);
-//   console.log(y);
-//   console.log(z);
-
-//   function inner() {
-//     var x = "inner's local x";
-
-//     console.log(x);
-//     console.log(y);
-//     console.log(z);
-//   }
-
-//   inner();
-// }
-
-// outer();
-
-// console.log(x);
-// console.log(z);
-
-// function foo() {
-//   console.log("global function foo");
-// }
-
-// function bar() {
-//   function foo() {
-//     console.log("local function foo");
-//   }
-
-//   foo();
-// }
-
-// bar();
-
-// var x = 1;
-// console.log(x);
-
-// if (true) {
-//   var x = 10;
-//   console.log(x);
-// }
-
-// console.log(x); // 10
-
-// var i = 10;
-
-// for (var i = 0; i < 5; i++) {
-//   console.log(i);
-// }
-
-// console.log(i);
-
-var x = 1;
-
-function foo() {
-  var x = 10;
-  bar();
+  // 체크하고 넣은 checkedArr 배열의 길이로 참 거짓을 확인을 한다.
+  if (checkedArr.length) {
+    return "FALSE";
+  } else {
+    return "TRUE";
+  }
 }
 
-function bar() {
-  console.log(x);
-}
+console.log(solution(`(){()}[]`));
+console.log(solution(`(){}]`));
+console.log(solution(`()`));
+console.log(solution(`()[]{}`));
+console.log(solution(`(]`));
+console.log(solution(`([)]`));
+console.log(solution(`{[]}`));
 
-foo();
-bar();
+// 배열을 반복문으로 돌린다. 데이터의 키와 비교를 하고 값과 일치하는지 확인
+// for (let i in stringArr) {
+//   if (openBracket.includes(stringArr[i])) {
+//     checkedArr.push(stringArr[i]);
+//     console.log("for in openBracket.includes(item) checkedArr", checkedArr);
+//   } else {
+//     if (checkedArr.includes(bracketsData[stringArr[i]])) {
+//       checkedArr.pop(bracketsData[stringArr[i]]);
+//       console.log(
+//         "for in checkedArr.includes(bracketsData[stringArr[i]])",
+//         checkedArr
+//       );
+//     } else {
+//       console.log(
+//         "checkedArr 안에 bracketsData 키와 일치하는 게 없을 때 넘어오는 else",
+//         bracketsData[stringArr[i]]
+//       );
+//       return false;
+//     }
+//   }
+// }
