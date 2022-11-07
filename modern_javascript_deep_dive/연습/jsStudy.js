3de07d2b8738eef2f087b8c3ea1aff1df16dedcb
홍현아 <hyeonahong@gmail.com>
2022-11-07 11:47:04 +0900

    14.3 전역 변수의 사용을 억제하는 방법 정리 : 네임스페이스 객체, 모듈 패턴

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -1,68 +1,22 @@
-function solution(string) {
-  // 검사할 기준을 데이터로 만든다
-  const bracketsData = {
-    ")": "(",
-    "}": "{",
-    "]": "[",
+var Counter = (function () {
+  // private 변수
+  var num = 0;
+
+  // 외부로 공개할 데이터나 메서드를 프로퍼티로 추가한 객체를 반환한다.
+  return {
+    increase() {
+      return ++num;
+    },
+    decrease() {
+      return --num;
+    },
   };
+})();
 
-  const openBracket = Object.values(bracketsData);
+// private 변수는 외부로 노출되지 않는다.
+console.log(Counter.num); // undefined
 
-  // 체크하고 넣을 빈 배열
-  const checkedArr = [];
-
-  // 매개변수로 들어온 문자열을 배열로 만든다
-  const stringArr = [...string];
-
-  stringArr.forEach((item) => {
-    // 매개변수로 들어온 배열의 요소(stringArr의 요소)가 데이터의 값(openBracket)과 같은지 확인하고 같으면 checkedArr에 넣는다. (추가)
-    if (openBracket.includes(item)) {
-      checkedArr.push(item);
-    } else {
-      // checkedArr의 요소와 data의 값이 일치하면 checkedArr에서 일치하는 요소를 지운다. (추가된 요소 삭제)
-      if (checkedArr.includes(bracketsData[item])) {
-        checkedArr.pop(bracketsData[item]);
-      } else {
-        // 모든 조건문을 돌고 나서 조건에 맞지 않는 요소는 집어넣기
-        checkedArr.push(item);
-      }
-    }
-  });
-
-  // 체크하고 넣은 checkedArr 배열의 길이로 참 거짓을 확인을 한다.
-  if (checkedArr.length) {
-    return "FALSE";
-  } else {
-    return "TRUE";
-  }
-}
-
-console.log(solution(`(){()}[]`));
-console.log(solution(`(){}]`));
-console.log(solution(`()`));
-console.log(solution(`()[]{}`));
-console.log(solution(`(]`));
-console.log(solution(`([)]`));
-console.log(solution(`{[]}`));
-
-// 배열을 반복문으로 돌린다. 데이터의 키와 비교를 하고 값과 일치하는지 확인
-// for (let i in stringArr) {
-//   if (openBracket.includes(stringArr[i])) {
-//     checkedArr.push(stringArr[i]);
-//     console.log("for in openBracket.includes(item) checkedArr", checkedArr);
-//   } else {
-//     if (checkedArr.includes(bracketsData[stringArr[i]])) {
-//       checkedArr.pop(bracketsData[stringArr[i]]);
-//       console.log(
-//         "for in checkedArr.includes(bracketsData[stringArr[i]])",
-//         checkedArr
-//       );
-//     } else {
-//       console.log(
-//         "checkedArr 안에 bracketsData 키와 일치하는 게 없을 때 넘어오는 else",
-//         bracketsData[stringArr[i]]
-//       );
-//       return false;
-//     }
-//   }
-// }
+console.log(Counter.increase()); // 1
+console.log(Counter.increase()); // 2
+console.log(Counter.decrease()); // 1
+console.log(Counter.decrease()); // 0
