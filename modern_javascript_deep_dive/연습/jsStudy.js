a003ce157f3b984e1922a8128624e225e1417117
홍현아 <hyeonahong@gmail.com>
2022-11-15 17:57:11 +0900

    18.2 함수 객체의 프로퍼티 : arguments를 제외한 부분 정리

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -1,34 +1,78 @@
-// 1. 함수는 무명의 리터럴로 생성할 수 있다.
-// 2. 함수는 변수에 저장할 수 있다.
-// 런타임(할당 단계)에 함수 리터럴이 평가되어 함수 객체가 생성되고 변수에 할당된다.
-const increase = function (num) {
-  return ++num;
-};
+// // 1. 함수는 무명의 리터럴로 생성할 수 있다.
+// // 2. 함수는 변수에 저장할 수 있다.
+// // 런타임(할당 단계)에 함수 리터럴이 평가되어 함수 객체가 생성되고 변수에 할당된다.
+// const increase = function (num) {
+//   return ++num;
+// };
 
-const decrease = function (num) {
-  return --num;
-};
+// const decrease = function (num) {
+//   return --num;
+// };
 
-// 2. 함수는 객체에 저장할 수 있다.
-const auxs = { increase, decrease };
+// // 2. 함수는 객체에 저장할 수 있다.
+// const auxs = { increase, decrease };
 
-// 3. 함수의 매개변수에 전달할 수 있다.
-// 4. 함수의 반환값으로 사용할 수 있다.
-function makeCounter(aux) {
-  let num = 0;
+// // 3. 함수의 매개변수에 전달할 수 있다.
+// // 4. 함수의 반환값으로 사용할 수 있다.
+// function makeCounter(aux) {
+//   let num = 0;
 
-  return function () {
-    num = aux(num);
-    return num;
-  };
+//   return function () {
+//     num = aux(num);
+//     return num;
+//   };
+// }
+
+// // 3. 함수는 매개변수에게 함수를 전달할 수 있다.
+// const increaser = makeCounter(auxs.increase);
+// console.log(increaser()); // 1
+// console.log(increaser()); // 2
+
+// // 3. 함수는 매개변수에게 함수를 전달할 수 있다.
+// const decreaser = makeCounter(auxs.decrease);
+// console.log(decreaser()); // -1
+// console.log(decreaser()); // -2
+
+// function square(number) {
+//   return number * number;
+// }
+
+// console.log(Object.getOwnPropertyDescriptors(square));
+// console.log(Object.getOwnPropertyDescriptor(Object.prototype, "__proto__"));
+
+// console.log(function () {}.hasOwnProperty("prototype"));
+// console.log({}.hasOwnProperty("prototype"));
+
+// const obj = { a: 1 };
+
+// console.log(obj.__proto__ === Object.prototype); // true
+
+// console.log(obj.hasOwnProperty("a")); // true
+// console.log(obj.hasOwnProperty("__proto__")); // false
+
+// // 기명 함수 표현식
+// var namedFunc = function foo() {};
+// console.log(namedFunc.name); // foo
+
+// // 익명 함수 표현식
+// var anonymousFunc = function () {};
+// // ES5: name 프로퍼티는 빈 문자열을 값으로 갖는다.
+// // ES6: name 프로퍼티는 함수 객체를 가리키는 변수 이름을 값으로 갖ㄴ드다.
+// console.log(anonymousFunc.name); // anonymousFunc
+
+// // 함수 선언문(Function declaration)
+// function bar() {}
+// console.log(bar.name); // bar
+
+function foo() {}
+console.log(foo.length); // 0
+
+function bar(x) {
+  return x;
 }
+console.log(bar.length); // 1
 
-// 3. 함수는 매개변수에게 함수를 전달할 수 있다.
-const increaser = makeCounter(auxs.increase);
-console.log(increaser()); // 1
-console.log(increaser()); // 2
-
-// 3. 함수는 매개변수에게 함수를 전달할 수 있다.
-const decreaser = makeCounter(auxs.decrease);
-console.log(decreaser()); // -1
-console.log(decreaser()); // -2
+function baz(x, y) {
+  return x * y;
+}
+console.log(baz.length); // 2
