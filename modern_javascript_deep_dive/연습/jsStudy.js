f44af389517ea20bf3344c24d7f0644f7ea88d0e
홍현아 <hyeonahong@gmail.com>
2022-11-15 15:03:09 +0900

    17.2 생성자 함수 정리

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -145,3 +145,92 @@
 // };
 
 // new obj.x(); // TypeError: obj.x is not a constructor
+
+// // 생성자 함수로 정의하지 않은 일반 함수
+// function add(x, y) {
+//   return x + y;
+// }
+
+// // 생성자 함수로 정의하지 않은 일반 함수를 new 연산자와 함께 호출
+// let inst = new add();
+
+// // 함수가 객체를 반환하지 않았으므로 반화문이 무시된다. 따라서 빈 객체가 생성되어 반환된다.
+// console.log(inst); // {}
+
+// // 객체를 반환하는 함수
+// function createUser(name, role) {
+//   return { name, role };
+// }
+
+// // 일반 함수를 new 연산자와 함께 호출
+// inst = new createUser("Lee", "admin");
+// console.log(inst); // { name: 'Lee', role: 'admin' }
+
+// // 생성자 함수
+// function Circle(radius) {
+//   this.radius = radius;
+//   this.getDiameter = function () {
+//     return 2 * this.radius;
+//   };
+// }
+
+// // new 연산자 없이 생성자 함수 호출하면 일반 함수로서 호출된다.
+// const circle = Circle(5);
+// console.log(circle); // undefined
+
+// // 일반 함수 내부의 this는 전역 객체 window를 가리킨다.
+// console.log(radius); // 5
+// console.log(getDiameter()); // 10
+
+// circle.getDiameter(); // TypeError: Cannot read properties of undefined
+
+// // 생성자 함수
+// function Circle(radius) {
+//   // 이 함수가 new 연산자와 함께 호출되지 않았다면 new.target은 undefined다.
+//   if (!new.target) {
+//     // new 연산자와 함께 생성자 함수를 재귀 호출하여 생성된 인스턴스를 반환한다.
+//     return new Circle(radius);
+//   }
+//   this.radius = radius;
+//   this.getDiameter = function () {
+//     return 2 * this.radius;
+//   };
+// }
+
+// // new 연산자 없이 생성자 함수를 호출하여도 new.target을 통해 생성자 함수로 호출된다.
+// const circle = Circle(5);
+// console.log(circle.getDiameter());
+
+// // Scope-Safe Constructor Pattern
+// function Circle(radius) {
+//   // 생성자 함수가 new  연산자와 함께 호출되면 함수의 선두에서 빈 객체를 생성하고
+//   // this에 바인딩한다. 이때 this와 Circle은 프로토타입에 의해 연결된다.
+
+//   // 이 함수가 new 연산자와 함께 호출되지 않았다면 이 시점의 this는 전역 객체 window를 가리킨다.
+//   // 즉, this와 Circle은 프로토타입에 의해 연결되지 않는다.
+//   if (!(this instanceof Circle)) {
+//     // new 연산자와 함께 호출하여 생성된 인스턴스를 반환한다.
+//     return new Circle(radius);
+//   }
+
+//   this.radius = radius;
+//   this.getDiameter = function () {
+//     return 2 * this.radius;
+//   };
+// }
+
+// // new 연산자 없이 생성자 함수를 호출하여도 생성자 함수로 호출된다.
+// const circle = Circle(5);
+// console.log(circle.getDiameter()); // 10
+
+let f = new Function("x", "return x ** x");
+console.log(f);
+
+const str = String(123);
+console.log(str, typeof str);
+
+const num = Number("123");
+console.log(num, typeof num);
+
+const bool = Boolean("true");
+console.log(bool, typeof bool);
