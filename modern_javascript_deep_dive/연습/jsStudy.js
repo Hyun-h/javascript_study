93116a5141df0b48a37e6f8e33b4646cfefc92d9
홍현아 <hyeonahong@gmail.com>
2022-11-15 15:47:54 +0900

    18.1 일급 객체 정리

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -1,236 +1,34 @@
-// // 빈 객체의 생성
-// const person = new Object();
+// 1. 함수는 무명의 리터럴로 생성할 수 있다.
+// 2. 함수는 변수에 저장할 수 있다.
+// 런타임(할당 단계)에 함수 리터럴이 평가되어 함수 객체가 생성되고 변수에 할당된다.
+const increase = function (num) {
+  return ++num;
+};
 
-// //  프로퍼티 추가
-// person.name = "Lee";
-// person.sayHello = function () {
-//   console.log(`Hi! My name is ${this.name}`);
-// };
+const decrease = function (num) {
+  return --num;
+};
 
-// console.log(person); // {name: 'Lee', sayHello: ƒ}
-// person.sayHello(); // Hi! My name is Lee
+// 2. 함수는 객체에 저장할 수 있다.
+const auxs = { increase, decrease };
 
-// // String 생성자 함수에 의한 String 객체 생성
-// const strObj = new String("Lee");
-// console.log(typeof strObj); // object
-// console.log(strObj); // String {'Lee'}
+// 3. 함수의 매개변수에 전달할 수 있다.
+// 4. 함수의 반환값으로 사용할 수 있다.
+function makeCounter(aux) {
+  let num = 0;
 
-// // Number 생성자 함수에 의한 Number 객체 생성
-// const numObj = new Number(123);
-// console.log(typeof numObj); // object
-// console.log(numObj); // Number {123}
+  return function () {
+    num = aux(num);
+    return num;
+  };
+}
 
-// // Boolean 생성자 함수에 의한 Boolean 객체 생성
-// const boolObj = new Boolean(true);
-// console.log(typeof boolObj); // object
-// console.log(boolObj); // Boolean {true}
+// 3. 함수는 매개변수에게 함수를 전달할 수 있다.
+const increaser = makeCounter(auxs.increase);
+console.log(increaser()); // 1
+console.log(increaser()); // 2
 
-// // Function 생성자 함수에 의한 Function 객체(함수) 생성
-// const func = new Function("x", "return x * x");
-// console.log(typeof func); // function
-// console.dir(func); // ƒ anonymous(x )
-
-// // Array 생성자 함수에 의한 Array 객체(배열) 생성
-// const arr = new Array(1, 2, 3);
-// console.log(typeof arr); // object
-// console.log(arr); // [1, 2, 3]
-
-// // RegExp 생성자 함수에 의한 RegExp 객체(정규 표현식) 생성
-// const regExp = new RegExp(/ad+c/i);
-// console.log(typeof regExp); // object
-// console.log(regExp); // /ad+c/i
-
-// // Data 생성자 함수에 의한 Date 객체 생성
-// const date = new Date();
-// console.log(typeof date); // object
-// console.log(date); // Fri Nov 11 2022 10:36:25 GMT+0900 (한국 표준시)
-
-// const circle1 = {
-//   radius: 5,
-//   getDiameter() {
-//     return 2 * this.radius;
-//   },
-// };
-
-// console.log(circle1.getDiameter());
-
-// const circle2 = {
-//   radius: 10,
-//   getDiameter() {
-//     return 2 * this.radius;
-//   },
-// };
-
-// console.log(circle2.getDiameter());
-
-// function Circle(radius) {
-//   // 생성자 함수 내부의 this는 생성자 함수가 생성할 인스턴스를 가리킨다.
-//   console.log(this);
-
-//   this.radius = radius;
-//   this.getDiameter = function () {
-//     return 2 * this.radius;
-//   };
-// }
-
-// // 인스턴스의 생성
-// const circle1 = new Circle(5); // 반지름이 5인 Circle 객체를 생성
-// const circle2 = new Circle(10); // 반지름이 10인 Circle 객체를 생성
-
-// console.log(circle1.getDiameter()); // 10
-// console.log(circle2.getDiameter()); // 20
-
-// // new 연산자와 함께 호출하지 않으면 생성자 함수로 동작하지 않는다.
-// // 즉, 일반 함수로서 호출된다.
-// const circle3 = Circle(15);
-
-// // 일반 함수로 호출된 Circle은 반환문이 없다. 그래서 암묵적으로 undefined를 반환한다.
-// console.log(circle3);
-
-// // 일반 함수로서 호출된 Circle 내의 this는 전역 객체를 가리킨다.
-// console.log(radius); // 15
-
-// function Circle(radius) {
-//   // 1. 암묵적으로 인스턴스가 생성되고 this에 바인딩 된다.
-//   console.log(this); // Circle {}
-
-//   // 2. this에 바인딩되어 있는 인스턴스를 초기화 한다.
-//   this.radius = radius;
-//   this.getDiameter = function () {
-//     return 2 * this.radius;
-//   };
-//   return 100;
-
-//   // 3. 완성된 인스턴스가 바인딩 된 this가 암묵적으로 반환된다.
-// }
-
-// // 인스턴스의 생성. Circle 생성자 함수는 암묵적으로 this를 반환한다.
-// const circle = new Circle(5);
-// console.log(circle); //
-
-// 함수는 객체다.
-// function foo() {}
-
-// // 함수는 객체이므로 프로퍼티를 소유할 수 있다.
-// foo.prop = 10;
-
-// // 함수는 객체이므로 메서드를 소유할 수 있다.
-// foo.method = function () {
-//   console.log(this.prop);
-// };
-
-// foo.method(); // 10
-
-// // 일반 함수 정의: 함수 선언문, 함수 표현식
-// function foo() {}
-// const bar = function () {};
-// // 프로퍼티 x의 값으로 할당된 것은 일반 함수로 정의된 함수다. 이는 메서드로 인정하지 않는다.
-// const baz = {
-//   x: function () {},
-// };
-
-// // 일반 함수로 정의된 함수만이 constructor다.
-// new foo(); // -> foo {}
-// new bar(); // -> bar {}
-// new baz.x(); // -> x {}
-
-// // 화살표 함수 정의
-// const arrow = () => {};
-
-// new arrow(); // TypeError: arrow is not a constructor
-
-// // 매서드 정의: ES6 메서드 축약 표현만 메서드로 인정한다.
-// const obj = {
-//   x() {},
-// };
-
-// new obj.x(); // TypeError: obj.x is not a constructor
-
-// // 생성자 함수로 정의하지 않은 일반 함수
-// function add(x, y) {
-//   return x + y;
-// }
-
-// // 생성자 함수로 정의하지 않은 일반 함수를 new 연산자와 함께 호출
-// let inst = new add();
-
-// // 함수가 객체를 반환하지 않았으므로 반화문이 무시된다. 따라서 빈 객체가 생성되어 반환된다.
-// console.log(inst); // {}
-
-// // 객체를 반환하는 함수
-// function createUser(name, role) {
-//   return { name, role };
-// }
-
-// // 일반 함수를 new 연산자와 함께 호출
-// inst = new createUser("Lee", "admin");
-// console.log(inst); // { name: 'Lee', role: 'admin' }
-
-// // 생성자 함수
-// function Circle(radius) {
-//   this.radius = radius;
-//   this.getDiameter = function () {
-//     return 2 * this.radius;
-//   };
-// }
-
-// // new 연산자 없이 생성자 함수 호출하면 일반 함수로서 호출된다.
-// const circle = Circle(5);
-// console.log(circle); // undefined
-
-// // 일반 함수 내부의 this는 전역 객체 window를 가리킨다.
-// console.log(radius); // 5
-// console.log(getDiameter()); // 10
-
-// circle.getDiameter(); // TypeError: Cannot read properties of undefined
-
-// // 생성자 함수
-// function Circle(radius) {
-//   // 이 함수가 new 연산자와 함께 호출되지 않았다면 new.target은 undefined다.
-//   if (!new.target) {
-//     // new 연산자와 함께 생성자 함수를 재귀 호출하여 생성된 인스턴스를 반환한다.
-//     return new Circle(radius);
-//   }
-//   this.radius = radius;
-//   this.getDiameter = function () {
-//     return 2 * this.radius;
-//   };
-// }
-
-// // new 연산자 없이 생성자 함수를 호출하여도 new.target을 통해 생성자 함수로 호출된다.
-// const circle = Circle(5);
-// console.log(circle.getDiameter());
-
-// // Scope-Safe Constructor Pattern
-// function Circle(radius) {
-//   // 생성자 함수가 new  연산자와 함께 호출되면 함수의 선두에서 빈 객체를 생성하고
-//   // this에 바인딩한다. 이때 this와 Circle은 프로토타입에 의해 연결된다.
-
-//   // 이 함수가 new 연산자와 함께 호출되지 않았다면 이 시점의 this는 전역 객체 window를 가리킨다.
-//   // 즉, this와 Circle은 프로토타입에 의해 연결되지 않는다.
-//   if (!(this instanceof Circle)) {
-//     // new 연산자와 함께 호출하여 생성된 인스턴스를 반환한다.
-//     return new Circle(radius);
-//   }
-
-//   this.radius = radius;
-//   this.getDiameter = function () {
-//     return 2 * this.radius;
-//   };
-// }
-
-// // new 연산자 없이 생성자 함수를 호출하여도 생성자 함수로 호출된다.
-// const circle = Circle(5);
-// console.log(circle.getDiameter()); // 10
-
-let f = new Function("x", "return x ** x");
-console.log(f);
-
-const str = String(123);
-console.log(str, typeof str);
-
-const num = Number("123");
-console.log(num, typeof num);
-
-const bool = Boolean("true");
-console.log(bool, typeof bool);
+// 3. 함수는 매개변수에게 함수를 전달할 수 있다.
+const decreaser = makeCounter(auxs.decrease);
+console.log(decreaser()); // -1
+console.log(decreaser()); // -2
