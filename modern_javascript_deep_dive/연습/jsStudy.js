0c1338a8f6e9567de1b0c7f64af5c0cbc6950bec
홍현아 <hyeonahong@gmail.com>
2022-11-16 14:58:49 +0900

    19.1 객체지향 프로그래밍 정리

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -103,9 +103,35 @@
 // console.log(sum(1, 2));
 // console.log(sum(1, 2, 3, 4, 5));
 
-function sum(...args) {
-  return args.reduce((pre, cur) => pre + cur, 0);
-}
+// function sum(...args) {
+//   return args.reduce((pre, cur) => pre + cur, 0);
+// }
 
-console.log(sum(1, 2));
-console.log(sum(1, 2, 3, 4, 5));
+// console.log(sum(1, 2));
+// console.log(sum(1, 2, 3, 4, 5));
+
+const circle = {
+  radius: 5, // 반지름
+
+  // 원의 지름: 2r
+  getDiameter() {
+    return 2 * this.radius;
+  },
+
+  // 원의 둘레: 2πr
+  getPerimeter() {
+    return 2 * Math.PI * this.radius;
+  },
+
+  // 원의 넓이: πrr
+  getArea() {
+    return Math.PI * this.radius ** 2;
+  },
+};
+
+console.log(circle);
+// {radius: 5, getDiameter: ƒ, getPerimeter: ƒ, getArea: ƒ}
+
+console.log(circle.getDiameter()); // 10
+console.log(circle.getPerimeter()); // 31.41592653589793
+console.log(circle.getArea()); // 78.53981633974483
