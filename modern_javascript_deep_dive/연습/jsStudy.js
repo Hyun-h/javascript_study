9f8408e5e64e72996580fd3e9fe61f13cdef6aa4
홍현아 <hyeonahong@gmail.com>
2022-11-16 16:41:53 +0900

    19.2 상속과 프로토타입 정리

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -110,28 +110,74 @@
 // console.log(sum(1, 2));
 // console.log(sum(1, 2, 3, 4, 5));
 
-const circle = {
-  radius: 5, // 반지름
+// const circle = {
+//   radius: 5, // 반지름
 
-  // 원의 지름: 2r
-  getDiameter() {
-    return 2 * this.radius;
-  },
+//   // 원의 지름: 2r
+//   getDiameter() {
+//     return 2 * this.radius;
+//   },
 
-  // 원의 둘레: 2πr
-  getPerimeter() {
-    return 2 * Math.PI * this.radius;
-  },
+//   // 원의 둘레: 2πr
+//   getPerimeter() {
+//     return 2 * Math.PI * this.radius;
+//   },
 
-  // 원의 넓이: πrr
-  getArea() {
-    return Math.PI * this.radius ** 2;
-  },
+//   // 원의 넓이: πrr
+//   getArea() {
+//     return Math.PI * this.radius ** 2;
+//   },
+// };
+
+// console.log(circle);
+// // {radius: 5, getDiameter: ƒ, getPerimeter: ƒ, getArea: ƒ}
+
+// console.log(circle.getDiameter()); // 10
+// console.log(circle.getPerimeter()); // 31.41592653589793
+// console.log(circle.getArea()); // 78.53981633974483
+
+// // 생성자 함수
+// function Circle(radius) {
+//   this.radius = radius;
+//   this.getArea = function () {
+//     // Math.PI는 원주율을 나타내는 상수다.
+//     return Math.PI * this.radius ** 2;
+//   };
+// }
+
+// // 반지름이 1인 인스턴스 생성
+// const circle1 = new Circle(1);
+// // 반지름이 2인 인스턴스 생성
+// const circle2 = new Circle(2);
+
+// // Circle 생성자 함수는 인스턴스를 생성할 때마다 동일한 동작을 하는
+// // getArea 메서드를 중복 생성하고 모든 인스턴스가 중복 소유한다.
+// // getArea 메서드는 하나만 생성하여 모드 인스턴스가 공유해서 사용하는 것이 바람직하다.
+// console.log(circle1.getArea === circle2.getArea); // false
+
+// console.log(circle1.getArea()); // 3.141592653589793
+// console.log(circle2.getArea()); // 12.566370614359172
+
+// 생성자 함수
+function Circle(radius) {
+  this.radius = radius;
+}
+
+// Circle 생성자 함수가 생성한 모든 인스턴스가 getArea 메서드를
+// 공유해서 사용할 수 있도록 프로토타입에 추가한다.
+// 프로토타입은 Circle 생성자 함수의 prototype 프로퍼티에 바인딩되어 있다.
+Circle.prototype.getArea = function () {
+  return Math.PI * this.radius ** 2;
 };
 
-console.log(circle);
-// {radius: 5, getDiameter: ƒ, getPerimeter: ƒ, getArea: ƒ}
+// 인스턴스 생성
+const circle1 = new Circle(1);
+const circle2 = new Circle(2);
 
-console.log(circle.getDiameter()); // 10
-console.log(circle.getPerimeter()); // 31.41592653589793
-console.log(circle.getArea()); // 78.53981633974483
+// Circle 생성자 함수가 생성한 모든 인스턴스는 부모 객체의 역할을 하는
+// 프로토타입 Circle.prototype으로부터 getArea 메서드를 상속받는다.
+// 즉, Circle 생성자 함수가 생성하는 모든 인스턴스는 하나의 getArea 메서드를 공유한다.
+console.log(circle1.getArea === circle2.getArea);
+
+console.log(circle1.getArea()); // 3.141592653589793
+console.log(circle2.getArea()); // 12.566370614359172
