f7cb7a1c43f39835c94b46412ca259a14c993b12
홍현아 <hyeonahong@gmail.com>
2022-11-17 17:08:44 +0900

    19.4 리터럴 표기법에 의해 생성된 객체의 생성자 함수와 프로토타입 정리

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -252,12 +252,46 @@
 // // non-constructor는 프로토타입을 생성하지 않는다.
 // console.log(obj.foo.prototype); // undefined
 
-// 생성자 함수
-function Person(name) {
-  this.name = name;
-}
+// // 생성자 함수
+// function Person(name) {
+//   this.name = name;
+// }
 
-const me = new Person("Lee");
+// const me = new Person("Lee");
 
-// me 객체의 생성자 함수는 Person이다.
-console.log(me.constructor === Person); // true
+// // me 객체의 생성자 함수는 Person이다.
+// console.log(me.constructor === Person); // true
+
+// // obj 객체를 생성한 생성자 함수는 Object다.
+// const obj = new Object();
+// console.log(obj.constructor === Object); // true
+
+// // add 함수 객체를 생성한 생성자 함수는 Function이다.
+// const add = new Function("a", "b", "return a + b");
+// console.log(add.constructor === Function); // true
+
+// // 생성자 함수
+// function Person(name) {
+//   this.name = name;
+// }
+
+// // me 객체를 생성한 생성자 함수는 Person이다.
+// const me = new Person("Lee");
+// console.log(me.constructor === Person); // true
+
+// obj 객체는 Object 생성자 함수로 생성한 객체가 아니라 객체 리터럴로 생성했다.
+const obj = {};
+
+// 하지만 obj 객체의 생성자 함수는 Object 생성자 함수다.
+console.log(obj.constructor === Object); // true
+
+// 함수 리터럴
+const add = function (a, b) {
+  return a + b;
+};
+
+// 배열 리터럴
+const arr = [1, 2, 3];
+
+// 정규 표현식 리터럴
+const regexp = /is/gi;
