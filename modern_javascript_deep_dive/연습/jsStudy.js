d8c724140e59dca2478010ee6b6562fba6822c4e
홍현아 <hyeonahong@gmail.com>
2022-12-09 12:29:35 +0900

    생성자 함수에 의한 프로토타입의 교체 정리

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -3,7 +3,19 @@ const Person = (function () {
     this.name = name;
   }
 
+  // 생상자 함수의 prototype 프로퍼티를 통해 프로토타입을 교체
   Person.prototype = {
-    sayHello() {},
+    // constructor 프로퍼티와 생성자 함수 간의 연결을 설정
+    constructor: Person,
+    sayHello() {
+      console.log(`Hi! My name is ${this.name}`);
+    },
   };
+
+  return Person;
 })();
+
+const me = new Person("Lee");
+
+console.log(me.constructor === Person); // true
+console.log(me.constructor === Object); // false
