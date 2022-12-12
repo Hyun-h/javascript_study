5a6a1828c06f231aecfe4c0c41a197f9d836afdb
홍현아 <hyeonahong@gmail.com>
2022-12-12 17:10:21 +0900

    Array.prototype.every 정리

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -1,21 +1,12 @@
-const Person = (function () {
-  function Person(name) {
-    this.name = name;
-  }
+const checkedObj = {
+  isParking: false,
+  isScreen: false,
+  isMembership: false,
+};
 
-  // 생상자 함수의 prototype 프로퍼티를 통해 프로토타입을 교체
-  Person.prototype = {
-    // constructor 프로퍼티와 생성자 함수 간의 연결을 설정
-    constructor: Person,
-    sayHello() {
-      console.log(`Hi! My name is ${this.name}`);
-    },
-  };
+const isInvalidCheck = Object.values(checkedObj).every((value) => !value);
 
-  return Person;
-})();
+console.log(isInvalidCheck);
 
-const me = new Person("Lee");
-
-console.log(me.constructor === Person); // true
-console.log(me.constructor === Object); // false
+if (isInvalidCheck)
+  return alert("타석타입은 무조건 하나 이상 체크되어야 합니다.");
