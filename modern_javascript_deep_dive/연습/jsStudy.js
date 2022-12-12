29071f34649553290a7b6977e87e450074030074
홍현아 <hyeonahong@gmail.com>
2022-12-12 18:00:26 +0900

    19.9 프로토타입의 교체 정리

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -1,12 +1,31 @@
-const checkedObj = {
-  isParking: false,
-  isScreen: false,
-  isMembership: false,
+function Person(name) {
+  this.name = name;
+}
+
+const me = new Person("Lee");
+
+// 프로토타입으로 교체할 객체
+const parent = {
+  // constructor 프로퍼티와 생성자 함수 간의 연결을 설정
+  constructor: Person,
+  sayHello() {
+    console.log(`Hi! My name is ${this.name}`);
+  },
 };
 
-const isInvalidCheck = Object.values(checkedObj).every((value) => !value);
+// 생성자 함수의 prototype 프로퍼티와 프로토타입 간의 연결을 설정
+Person.prototype = parent;
 
-console.log(isInvalidCheck);
+// me 객체의 프로토타입을 parent 객체로 교체한다.
+Object.setPrototypeOf(me, parent);
+// 위 코드는 아래의 코드와 동일하게 동작한다.
+// me.__proto__ = parent;
 
-if (isInvalidCheck)
-  return alert("타석타입은 무조건 하나 이상 체크되어야 합니다.");
+me.sayHello(); // Hi! My name is Lee
+
+// constructor 프로퍼티가 생성자 함수를 가리킨다.
+console.log(me.constructor === Person); // true
+console.log(me.constructor === Object); // false
+
+// 생성자 함수의 prototype 프로퍼티가 교체된 프로토타입을 가리킨다.
+console.log(Person.prototype === Object.getPrototypeOf(me)); // true
