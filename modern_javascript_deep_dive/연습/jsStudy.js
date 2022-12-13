e83238959772fc852461398580a9f4c539435269
홍현아 <hyeonahong@gmail.com>
2022-12-13 15:24:50 +0900

    19.10 instanceof 연산자 정리

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -1,31 +1,12 @@
+// 생성자 함수
 function Person(name) {
   this.name = name;
 }
 
 const me = new Person("Lee");
 
-// 프로토타입으로 교체할 객체
-const parent = {
-  // constructor 프로퍼티와 생성자 함수 간의 연결을 설정
-  constructor: Person,
-  sayHello() {
-    console.log(`Hi! My name is ${this.name}`);
-  },
-};
+// Person.prototype이 me 객체의 프로토타입 체인 사엥 존재하므로 true로 평가된다.
+console.log(me instanceof Person); // true
 
-// 생성자 함수의 prototype 프로퍼티와 프로토타입 간의 연결을 설정
-Person.prototype = parent;
-
-// me 객체의 프로토타입을 parent 객체로 교체한다.
-Object.setPrototypeOf(me, parent);
-// 위 코드는 아래의 코드와 동일하게 동작한다.
-// me.__proto__ = parent;
-
-me.sayHello(); // Hi! My name is Lee
-
-// constructor 프로퍼티가 생성자 함수를 가리킨다.
-console.log(me.constructor === Person); // true
-console.log(me.constructor === Object); // false
-
-// 생성자 함수의 prototype 프로퍼티가 교체된 프로토타입을 가리킨다.
-console.log(Person.prototype === Object.getPrototypeOf(me)); // true
+// Object.prototype이 me 객체의 프로토타입 체인 상에 존재하므로 true로 평가된다.
+console.log(me instanceof Object); // true
