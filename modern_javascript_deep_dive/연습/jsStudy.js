0d1035a1577ecdacad05bd2624e72db357a32f47
홍현아 <hyeonahong@gmail.com>
2022-12-15 10:10:04 +0900

    19.13 프로퍼티 존재 확인 : Object.prototype.hasOwnProperty

--- modern_javascript_deep_dive/연습/jsStudy.js
+++ modern_javascript_deep_dive/연습/jsStudy.js
@@ -14,3 +14,7 @@ console.log("toString" in person);
 
 console.log(Reflect.has(person, "name")); // true
 console.log(Reflect.has(person, "toString")); // true
+
+console.log(person.hasOwnProperty("name")); // true
+console.log(person.hasOwnProperty("age")); // false
+console.log(person.hasOwnProperty("toString")); // false
